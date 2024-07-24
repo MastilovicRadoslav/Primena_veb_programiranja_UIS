3e414729363fd9a1f3978163c324e2f16a63b21c
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2024-07-24 11:51:25 +0200

    taxi-app frontend

---
+++ Resenje/taxi-app/src/App.js
@@ -0,0 +1,21 @@
+import 'process/browser'; // or require('process/browser');
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <>
+        <Router>
+            <Routes>
+            </Routes>
+        </Router>
+    </>
+  );
+}
+
+export default App;
