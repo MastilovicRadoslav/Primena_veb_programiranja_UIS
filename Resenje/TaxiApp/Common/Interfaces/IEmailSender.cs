47e70e72a6ab03db608e5e81f2a78df21dab0556
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2024-07-26 19:25:26 +0200

    Email Notification Basic

---
+++ Resenje/TaxiApp/Common/Interfaces/IEmailSender.cs
@@ -0,0 +1,7 @@
+﻿namespace Common.Interfaces
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
---
+++ Resenje/TaxiApp/Common/Models/EmailSender.cs
@@ -0,0 +1,31 @@
+﻿using Common.Interfaces;
+using System.Net;
+using System.Net.Mail;
+
+namespace Common.Models
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            var client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                EnableSsl = true,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential("drsprojekat2023@gmail.coms", "Bulevar Despota Stefana 7")
+            };
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress("drsprojekat2023@gmail.com"),
+                Subject = subject,
+                Body = message,
+                IsBodyHtml = true
+            };
+
+            mailMessage.To.Add(email);
+
+            return client.SendMailAsync(mailMessage);
+        }
+    }
+}
