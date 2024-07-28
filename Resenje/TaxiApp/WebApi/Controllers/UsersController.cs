a6610ab915063f7f7387e555b2be59c5d4436c40
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2024-07-28 20:08:29 +0200

    UsersController

--- Resenje/TaxiApp/WebApi/Controllers/UsersController.cs
+++ Resenje/TaxiApp/WebApi/Controllers/UsersController.cs
@@ -1,10 +1,85 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
+﻿using Common.DTOs;
+using Common.Interfaces;
+using Common.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
 namespace WebApi.Controllers
 {
     [Route("api/[controller]/[action]")]
     [ApiController]
     public class UsersController : ControllerBase
     {
+
+        private IConfiguration _configuration;
+        private readonly IEmailSender emailSender;
+
+        public UsersController(IConfiguration config, IEmailSender emailSender)
+        {
+            _configuration = config;
+            this.emailSender = emailSender;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromForm] UserForRegister userData)
+        {
+            if (string.IsNullOrEmpty(userData.Email) || !CheckEmail(userData.Email)) return BadRequest("Invalid email format");
+            if (string.IsNullOrEmpty(userData.Password)) return BadRequest("Password cannot be null or empty");
+            if (string.IsNullOrEmpty(userData.Username)) return BadRequest("Username cannot be null or empty");
+            if (string.IsNullOrEmpty(userData.FirstName)) return BadRequest("First name cannot be null or empty");
+            if (string.IsNullOrEmpty(userData.LastName)) return BadRequest("Last name cannot be null or empty");
+            if (string.IsNullOrEmpty(userData.Address)) return BadRequest("Address cannot be null or empty");
+            if (string.IsNullOrEmpty(userData.TypeOfUser)) return BadRequest("Type of user must be selected!");
+            if (string.IsNullOrEmpty(userData.Birthday)) return BadRequest("Birthday need to be selected!");
+            if (userData.ImageUrl.Length == 0) return BadRequest("You need to send image while doing registration!");
+            try
+            {
+                if (true) return Ok($"Successfully registered new User: {userData.Username}");
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "An error occurred while registering new User");
+            }
+        }
+
+
+        [HttpGet]
+        public async Task<List<FullUserDTO>> GetUsers()
+        {
+
+            try
+            {
+                return new List<FullUserDTO>(); // Return an empty list or handle the error as needed
+            }
+            catch (Exception ex)
+            {
+                // Log the exception
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                return new List<FullUserDTO>(); // Return an empty list or handle the error as needed
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginUserDTO user)
+        {
+            if (string.IsNullOrEmpty(user.Email) || !CheckEmail(user.Email)) return BadRequest("Invalid email format");
+            if (string.IsNullOrEmpty(user.Password)) return BadRequest("Password cannot be null or empty");
+
+            try
+            {
+                return StatusCode(500, "An error occurred while login User");
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred while login User");
+            }
+        }
+
+        private bool CheckEmail(string email)
+        {
+            const string pattern = @"^[^\s@]+@[^\s@]+\.[^\s@]+$";
+            return Regex.IsMatch(email, pattern);
+        }
+
     }
 }
