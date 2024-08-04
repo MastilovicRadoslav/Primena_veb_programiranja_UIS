9877d2c6e65843a420485ad84d9be40a42df4abd
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2024-08-04 12:35:57 +0200

    UsersController

--- Resenje/TaxiApp/WebApi/Controllers/UsersController.cs
+++ Resenje/TaxiApp/WebApi/Controllers/UsersController.cs
@@ -2,7 +2,15 @@
 using Common.Interfaces;
 using Common.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.ServiceFabric.Services.Client;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+using System.Fabric;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Text.RegularExpressions;
+
 namespace WebApi.Controllers
 {
     [Route("api/[controller]/[action]")]
@@ -11,16 +19,16 @@ namespace WebApi.Controllers
     {
 
         private IConfiguration _configuration;
-        private readonly IEmailSender emailSender;
+        private readonly IEmailService emailSender;
 
-        public UsersController(IConfiguration config, IEmailSender emailSender)
+        public UsersController(IConfiguration config, IEmailService emailSender)
         {
             _configuration = config;
             this.emailSender = emailSender;
         }
 
         [HttpPost]
-        public async Task<IActionResult> Register([FromForm] UserForRegister userData)
+        public async Task<IActionResult> Register([FromForm] UserRegistrationModel userData)
         {
             if (string.IsNullOrEmpty(userData.Email) || !CheckEmail(userData.Email)) return BadRequest("Invalid email format");
             if (string.IsNullOrEmpty(userData.Password)) return BadRequest("Password cannot be null or empty");
@@ -33,7 +41,24 @@ namespace WebApi.Controllers
             if (userData.ImageUrl.Length == 0) return BadRequest("You need to send image while doing registration!");
             try
             {
-                if (true) return Ok($"Successfully registered new User: {userData.Username}");
+
+                UserModel userForRegister = new UserModel(userData);
+
+                var fabricClient = new FabricClient();
+                bool result = false;
+
+                var partitionList = await fabricClient.QueryManager.GetPartitionListAsync(new Uri("fabric:/TaxiApp/UsersService"));
+                foreach (var partition in partitionList)
+                {
+                    var partitionKey = new ServicePartitionKey(((Int64RangePartitionInformation)partition.PartitionInformation).LowKey);
+                    var proxy = ServiceProxy.Create<IUserService>(new Uri("fabric:/TaxiApp/UsersService"), partitionKey);
+                    result = await proxy.addNewUser(userForRegister);
+                }
+
+                if (result) return Ok($"Successfully registered new User: {userData.Username}");
+                else return StatusCode(409, "User already exists in database!");
+
+
             }
             catch (Exception)
             {
@@ -43,18 +68,30 @@ namespace WebApi.Controllers
 
 
         [HttpGet]
-        public async Task<List<FullUserDTO>> GetUsers()
+        public async Task<List<UserDetailsDTO>> GetUsers()
         {
 
             try
             {
-                return new List<FullUserDTO>(); // Return an empty list or handle the error as needed
+                var fabricClient = new FabricClient();
+                var result = new List<UserDetailsDTO>();
+
+                var partitionList = await fabricClient.QueryManager.GetPartitionListAsync(new Uri("fabric:/TaxiApp/UsersService"));
+                foreach (var partition in partitionList)
+                {
+                    var partitionKey = new ServicePartitionKey(((Int64RangePartitionInformation)partition.PartitionInformation).LowKey);
+                    var proxy = ServiceProxy.Create<IUserService>(new Uri("fabric:/TaxiApp/UsersService"), partitionKey);
+                    var partitionResult = await proxy.listUsers();
+                    result.AddRange(partitionResult);
+                }
+
+                return result;
             }
             catch (Exception ex)
             {
                 // Log the exception
                 Console.WriteLine($"An error occurred: {ex.Message}");
-                return new List<FullUserDTO>(); // Return an empty list or handle the error as needed
+                return new List<UserDetailsDTO>(); // Return an empty list or handle the error as needed
             }
         }
 
@@ -66,8 +103,52 @@ namespace WebApi.Controllers
 
             try
             {
-                return StatusCode(500, "An error occurred while login User");
+                var fabricClient = new FabricClient();
+                LogedUserDTO result = null; // Initialize result to null
 
+                var partitionList = await fabricClient.QueryManager.GetPartitionListAsync(new Uri("fabric:/TaxiApp/UsersService"));
+                foreach (var partition in partitionList)
+                {
+                    var partitionKey = new ServicePartitionKey(((Int64RangePartitionInformation)partition.PartitionInformation).LowKey);
+                    var proxy = ServiceProxy.Create<IUserService>(new Uri("fabric:/TaxiApp/UsersService"), partitionKey);
+                    var partitionResult = await proxy.loginUser(user);
+
+                    if (partitionResult != null)
+                    {
+                        result = partitionResult;
+                        break;
+                    }
+                }
+
+                if (result != null)
+                {
+                    var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+                    var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+                    List<Claim> claims = new List<Claim>();
+                    claims.Add(new Claim("MyCustomClaim", result.Roles.ToString()));
+
+                    var Sectoken = new JwtSecurityToken(_configuration["Jwt:Issuer"],
+                        _configuration["Jwt:Issuer"],
+                        claims,
+                        expires: DateTime.Now.AddMinutes(360),
+                        signingCredentials: credentials);
+
+                    var token = new JwtSecurityTokenHandler().WriteToken(Sectoken);
+
+                    var response = new
+                    {
+                        token = token,
+                        user = result,
+                        message = "Login successful"
+                    };
+
+                    return Ok(response);
+                }
+                else
+                {
+                    return BadRequest("Incorrect email or password");
+                }
             }
             catch (Exception ex)
             {
