7e2a69de6789e874e12c84a4e83d75abb14bde96
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2024-08-05 16:10:58 +0200

    DrivingService and IDriveService basic settings

--- Resenje/TaxiApp/DrivingService/DrivingService.cs
+++ Resenje/TaxiApp/DrivingService/DrivingService.cs
@@ -1,3 +1,4 @@
+using Common.Interfaces;
 using Microsoft.ServiceFabric.Data.Collections;
 using Microsoft.ServiceFabric.Services.Communication.Runtime;
 using Microsoft.ServiceFabric.Services.Runtime;
@@ -8,11 +9,15 @@ namespace DrivingService
     /// <summary>
     /// An instance of this class is created for each service replica by the Service Fabric runtime.
     /// </summary>
-    internal sealed class DrivingService : StatefulService
+    internal sealed class DrivingService : StatefulService, IDriveService
     {
+        DrivingDataRepository dataRepo;
         public DrivingService(StatefulServiceContext context)
             : base(context)
-        { }
+        {
+            dataRepo = new DrivingDataRepository("DrivingTable");
+        }
+
 
         /// <summary>
         /// Optional override to create listeners (e.g., HTTP, Service Remoting, WCF, etc.) for this service replica to handle client or user requests.
