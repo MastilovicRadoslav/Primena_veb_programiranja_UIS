76883a2e0eb3ad3d56fab7867e5429e481700c7c
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2024-08-06 09:55:00 +0200

    UserUpdateModel

---
+++ Resenje/TaxiApp/Common/Models/UserUpdateModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Common.Models
+{
+    public class UserUpdateModel
+    {
+        public string? FirstName { get; set; }
+
+        public string? PreviousEmail { get; set; }
+        public string? LastName { get; set; }
+        public string? Birthday { get; set; }
+        public string? Address { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public IFormFile? ImageUrl { get; set; }
+        public string? Username { get; set; }
+
+        public Guid Id { get; set; }
+        public UserUpdateModel()
+        {
+        }
+    }
+}
