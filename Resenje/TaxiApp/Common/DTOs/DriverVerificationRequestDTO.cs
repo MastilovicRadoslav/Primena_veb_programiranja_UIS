2d5436139098123205509e6aaddc3d97cbcb2e5c
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2024-08-07 09:06:21 +0200

    DriverVerificationRequestDTO

---
+++ Resenje/TaxiApp/Common/DTOs/DriverVerificationRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Common.DTOs
+{
+    public class DriverVerificationRequestDTO
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+
+        public string Action { get; set; }
+    }
+}
