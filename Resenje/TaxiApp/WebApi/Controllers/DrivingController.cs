3f2f5acf3c5c694bb89ce0455d74781bb69828a3
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2024-08-08 13:33:02 +0200

    DrivingController

--- Resenje/TaxiApp/WebApi/Controllers/DrivingController.cs
+++ Resenje/TaxiApp/WebApi/Controllers/DrivingController.cs
@@ -1,16 +1,10 @@
-﻿using Common.DTOs;
-using Common.Interfaces;
+﻿using Common.Interfaces;
 using Common.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.ServiceFabric.Services.Client;
 using Microsoft.ServiceFabric.Services.Remoting.Client;
 using System.Fabric;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
-using System.Text;
-using System.Text.RegularExpressions;
 
 namespace WebApi.Controllers
 {
@@ -18,5 +12,55 @@ namespace WebApi.Controllers
     [ApiController]
     public class DrivingController : ControllerBase
     {
+
+        [Authorize(Policy = "Rider")]
+        [HttpPut]
+        public async Task<IActionResult> AcceptSuggestedDrive([FromBody] AcceptedRoadTripModel acceptedRoadTrip)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(acceptedRoadTrip.Destination)) return BadRequest("You must send destination!");
+                if (string.IsNullOrEmpty(acceptedRoadTrip.CurrentLocation)) return BadRequest("You must send location!");
+                if (acceptedRoadTrip.Accepted == true) return BadRequest("Ride cannot be automaticaly accepted!");
+                if (acceptedRoadTrip.Price == 0.0 || acceptedRoadTrip.Price < 0.0) return BadRequest("Invalid price!");
+
+
+                var fabricClient = new FabricClient();
+                RoadTripModel result = null;
+                RoadTripModel tripFromRider = new RoadTripModel(acceptedRoadTrip.CurrentLocation, acceptedRoadTrip.Destination, acceptedRoadTrip.RiderId, acceptedRoadTrip.Price, acceptedRoadTrip.Accepted, acceptedRoadTrip.MinutesToDriverArrive);
+                var partitionList = await fabricClient.QueryManager.GetPartitionListAsync(new Uri("fabric:/TaxiApp/DrivingService"));
+                foreach (var partition in partitionList)
+                {
+                    var partitionKey = new ServicePartitionKey(((Int64RangePartitionInformation)partition.PartitionInformation).LowKey);
+                    var proxy = ServiceProxy.Create<IDriveService>(new Uri("fabric:/TaxiApp/DrivingService"), partitionKey);
+                    var partitionResult = await proxy.AcceptRoadTrip(tripFromRider);
+                    if (partitionResult != null)
+                    {
+                        result = partitionResult;
+                        break;
+                    }
+                }
+
+                if (result != null)
+                {
+                    var response = new
+                    {
+                        Drive = result,
+                        message = "Successfully scheduled"
+                    };
+                    return Ok(response);
+                }
+                else
+                {
+                    return BadRequest("You already submited ticked!");
+                }
+
+
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "An error occurred while accepting new drive!");
+            }
+        }
     }
 }
