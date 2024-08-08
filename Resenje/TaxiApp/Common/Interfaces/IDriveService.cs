ded89d60d8067013d4ffc7723543ef42e9de6b8e
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2024-08-08 13:33:44 +0200

    IDriveService

--- Resenje/TaxiApp/Common/Interfaces/IDriveService.cs
+++ Resenje/TaxiApp/Common/Interfaces/IDriveService.cs
@@ -1,6 +1,11 @@
-﻿namespace Common.Interfaces
+﻿using Common.Models;
+using Microsoft.ServiceFabric.Services.Remoting;
+using System.ServiceModel;
+namespace Common.Interfaces
 {
-    public interface IDriveService
+    public interface IDriveService : IService
     {
+        [OperationContract]
+        Task<RoadTripModel> AcceptRoadTrip(RoadTripModel trip);
     }
 }
