acf8eb4672479499b42ee179a4d0424079f706c3
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2024-08-08 16:53:04 +0200

    IPredictionService

---
+++ Resenje/TaxiApp/Common/Interfaces/IPredictionService.cs
@@ -0,0 +1,13 @@
+﻿using Common.Models;
+using Microsoft.ServiceFabric.Services.Remoting;
+using System.ServiceModel;
+
+namespace Common.Interfaces
+{
+    [ServiceContract]
+    public interface IPredictionService : IService
+    {
+        [OperationContract]
+        Task<PredictionModel> GetPredictionPrice(string currentLocation, string destination);
+    }
+}
