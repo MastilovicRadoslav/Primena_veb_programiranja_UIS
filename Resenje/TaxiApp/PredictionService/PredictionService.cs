15ca2b6156888a6ad3c1aecacb1d4b941e1fc10c
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2024-08-08 16:52:21 +0200

    PredictionService

--- Resenje/TaxiApp/PredictionService/PredictionService.cs
+++ Resenje/TaxiApp/PredictionService/PredictionService.cs
@@ -1,4 +1,7 @@
+using Common.Interfaces;
+using Common.Models;
 using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Remoting.Runtime;
 using Microsoft.ServiceFabric.Services.Runtime;
 using System.Fabric;
 
@@ -7,20 +10,33 @@ namespace PredictionService
     /// <summary>
     /// An instance of this class is created for each service instance by the Service Fabric runtime.
     /// </summary>
-    internal sealed class PredictionService : StatelessService
+    internal sealed class PredictionService : StatelessService, IPredictionService
     {
         public PredictionService(StatelessServiceContext context)
             : base(context)
         { }
 
+        public async Task<PredictionModel> GetPredictionPrice(string currentLocation, string destination)
+        {
+            double rangeMin = 5.0;
+            double rangeMax = 20.0;
+
+            Random r = new Random();
+            double price = rangeMin + (rangeMax - rangeMin) * r.NextDouble();
+
+            // Create TimeSpan objects
+            TimeSpan estimatedTimeMin = new TimeSpan(0, 1, 0); // 1 minute
+            TimeSpan estimatedTimeMax = new TimeSpan(0, 2, 0); // 2 minutes
+
+            return new PredictionModel(price, estimatedTimeMin, estimatedTimeMax);
+
+        }
         /// <summary>
         /// Optional override to create listeners (e.g., TCP, HTTP) for this service replica to handle client or user requests.
         /// </summary>
         /// <returns>A collection of listeners.</returns>
         protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
-        {
-            return new ServiceInstanceListener[0];
-        }
+           => this.CreateServiceRemotingInstanceListeners(); //DODATO
 
         /// <summary>
         /// This is the main entry point for your service instance.
