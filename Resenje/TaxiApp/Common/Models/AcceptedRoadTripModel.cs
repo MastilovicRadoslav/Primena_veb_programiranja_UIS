507bc01e017b5c8b084fd8cfd7f95b77e50b0d37
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2024-08-09 09:52:47 +0200

    AcceptedRoadTripModel and TripModel

---
+++ Resenje/TaxiApp/Common/Models/AcceptedRoadTripModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Common.Models
+{
+    public class AcceptedRoadTripModel
+    {
+        public string Destination { get; set; } // ovo dobija 
+        public string CurrentLocation { get; set; } // ovo dobija 
+        public Guid RiderId { get; set; } // ovo dobija 
+        public double Price { get; set; } // ovo dobija 
+        public bool Accepted { get; set; } // ovo dobija 
+
+        public int MinutesToDriverArrive { get; set; }
+        public AcceptedRoadTripModel() { }
+    }
+}
---
+++ Resenje/TaxiApp/Common/Models/TripModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Common.Models
+{
+    public class TripModel
+    {
+        public string Destination { get; set; }
+        public string CurrentLocation { get; set; }
+
+        public TripModel()
+        {
+        }
+    }
+}
