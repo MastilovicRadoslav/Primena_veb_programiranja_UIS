dc0929a13e4f2193169e4f1d5af5e382cf8a3df7
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2024-08-09 09:51:24 +0200

    PredictionModel

---
+++ Resenje/TaxiApp/Common/Models/PredictionModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace Common.Models
+{
+    [DataContract]
+    public class PredictionModel
+    {
+        [DataMember]
+        public double PredictionPrice { get; set; }
+        [DataMember]
+        public TimeSpan DriversArivalSeconds { get; set; }
+
+        [DataMember]
+        public TimeSpan RideTime { get; set; }
+
+        public PredictionModel(double predictionPrice, TimeSpan driversArivalSeconds, TimeSpan rideTime)
+        {
+            PredictionPrice = predictionPrice;
+            DriversArivalSeconds = driversArivalSeconds;
+            RideTime = rideTime;
+        }
+    }
+}
