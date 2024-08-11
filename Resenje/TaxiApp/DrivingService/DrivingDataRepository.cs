baf9bd4dfae3b2a7b0ca75649c8b5a468fe3e579
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2024-08-11 10:06:05 +0200

    DrivingDataRepository

--- Resenje/TaxiApp/DrivingService/DrivingDataRepository.cs
+++ Resenje/TaxiApp/DrivingService/DrivingDataRepository.cs
@@ -54,6 +54,48 @@ namespace DrivingService
             }
         }
 
+        public IEnumerable<RoadTripEntity> GetAllTrips()
+        {
+            var q = new TableQuery<RoadTripEntity>();
+            var qRes = Trips.ExecuteQuerySegmentedAsync(q, null).GetAwaiter().GetResult();
+            return qRes.Results;
+        }
+        public async Task RateTrip(Guid tripId)
+        {
+            TableQuery<RoadTripEntity> rideQuery = new TableQuery<RoadTripEntity>()
+        .Where(TableQuery.GenerateFilterConditionForGuid("TripId", QueryComparisons.Equal, tripId));
+            TableQuerySegment<RoadTripEntity> queryResult = await Trips.ExecuteQuerySegmentedAsync(rideQuery, null);
+
+            if (queryResult.Results.Count > 0)
+            {
+                RoadTripEntity trip = queryResult.Results[0];
+                trip.IsRated = true;
+                var operation = TableOperation.Replace(trip);
+                await Trips.ExecuteAsync(operation);
+            }
+
+        }
+
+        public async Task<bool> FinishTrip(Guid tripId)
+        {
+            TableQuery<RoadTripEntity> rideQuery = new TableQuery<RoadTripEntity>()
+        .Where(TableQuery.GenerateFilterConditionForGuid("TripId", QueryComparisons.Equal, tripId));
+            TableQuerySegment<RoadTripEntity> queryResult = await Trips.ExecuteQuerySegmentedAsync(rideQuery, null);
+
+            if (queryResult.Results.Count > 0)
+            {
+                RoadTripEntity trip = queryResult.Results[0];
+                trip.IsFinished = true;
+                var operation = TableOperation.Replace(trip);
+                await Trips.ExecuteAsync(operation);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
         public CloudStorageAccount CloudAcc { get => cloudAcc; set => cloudAcc = value; }
         public CloudTableClient TableClient { get => tableClient; set => tableClient = value; }
         public CloudTable Trips { get => _trips; set => _trips = value; }
