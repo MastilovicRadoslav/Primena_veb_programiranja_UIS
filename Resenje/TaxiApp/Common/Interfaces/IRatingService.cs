fee3c7b9af000c53f8ccbb38456f2358504db7e9
Radoslav Mastilović <122049689+MastilovicRadoslav@users.noreply.github.com>
2024-08-13 09:27:55 +0200

    IRatingService

---
+++ Resenje/TaxiApp/Common/Interfaces/IRatingService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ServiceFabric.Services.Remoting;
+using System.ServiceModel;
+
+namespace Common.Interfaces
+{
+    [ServiceContract]
+    public interface IRatingService : IService
+    {
+        [OperationContract]
+        Task<bool> AddRating(Guid driverId, int rating);
+
+    }
+}
